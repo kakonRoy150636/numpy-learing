5c0fadcffb242f32f3fb413293126efbf9593430
KakonRoy150636 <kakonroy150636@gmail.com>
2026-08-15 12:06:09 -0500

    index and slicing

---
+++ index_and_sliging.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+#1D
+arr1d = np.array([10, 11, 12, 13,14])
+print(arr1d[1])
+print(arr1d[-1])
+print(arr1d[4])
+
+#2d
+arr2d = np.array([[1,2,3],[4,5,6],[7,8,8]])
+print(arr2d[2,2])
